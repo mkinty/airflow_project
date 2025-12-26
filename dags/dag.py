b670e2dafc3d44bd8e39d900ce67ad9da564b88a
Moustapha KINTY <kintymoustapha@gmail.com>
2025-12-26 16:57:27 +0100

    ajouter une task à un dag

--- dags/dag.py
+++ dags/dag.py
@@ -1,12 +1,19 @@
+from airflow.decorators import task
 from airflow.models.dag import dag
 from airflow.operators.empty import EmptyOperator
+
+
+@task()
+def get_data():
+    print("Téléchargement des données")
 
 
 @dag()
 def flights_pipeline():
     (
-        EmptyOperator(task_id="start")
-        >> EmptyOperator(task_id="end")
+            EmptyOperator(task_id="start")
+            >> get_data()
+            >> EmptyOperator(task_id="end")
     )
 
 
