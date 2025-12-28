0ee592744c32529e15d76f00649da37388e7c260
Moustapha KINTY <kintymoustapha@gmail.com>
2025-12-28 15:59:45 +0100

    transformer une task pour qu'elle retourn un xcom

--- dags/dag.py
+++ dags/dag.py
@@ -30,36 +30,45 @@ OPEN_SKY_COLUMNS = [
 ALL_STATES_URL = "https://opensky-network.org/api/states/all?extended=true"
 
 # Chemins absolus dans le container
-DATA_FILE_PATH = "/opt/airflow/dags/data/data.json"
+# DATA_FILE_PATH = "/opt/airflow/dags/data/data.json"
 DB_FILE_PATH = "/opt/airflow/dags/data/bdd_airflow.duckdb"
 
 
 @task()
-def get_flight_data(columns, url, data_file_path):
+def get_flight_data(columns, url):
     req = requests.get(url)
     response = req.json()
 
     if req.status_code != 200:
         raise Exception(f"Erreur API OpenSky ({req.status_code}) : {response.get('message')}")
 
+    timestamp = response.get("time", None)
     states_list = response.get("states", [])
     states_json = [dict(zip(columns, state)) for state in states_list]
 
+    data_file_path = f"/opt/airflow/dags/data/data_{timestamp}.json"
     with open(data_file_path, "w") as file:
         json.dump(states_json, file)
     print(f"Données téléchargées dans {data_file_path}")
+    return data_file_path
 
 
 @task()
-def load_from_file(data_file_path, db_file_path):
+def load_from_file(db_file_path, ti=None):
     conn = None
+    data_file_path = ti.xcom_pull(task_ids='get_flight_data', key='return_value')
     try:
         conn = duckdb.connect(db_file_path)
         # Créer la table si elle n'existe pas et insérer les données
+        # conn.sql(f"""
+        # CREATE TABLE IF NOT EXISTS opskynetwork_brute AS
+        # SELECT * FROM read_json_auto('{data_file_path}')
+        # """)
+        # Une fois la table créée, il faut simplement inserer les novelles données
         conn.sql(f"""
-        CREATE TABLE IF NOT EXISTS opskynetwork_brute AS
-        SELECT * FROM read_json_auto('{data_file_path}')
-        """)
+                INSERT INTO bdd_airflow.main.opskynetwork_brute
+                (SELECT * FROM read_json_auto('{data_file_path}'))
+                """)
         print(f"Données insérées dans {db_file_path}")
     except Exception as e:
         print("Erreur DuckDB:", e)
@@ -107,8 +116,8 @@ def check_duplicates(db_file_path):
 def flights_pipeline():
     (
             EmptyOperator(task_id="start")
-            >> get_flight_data(OPEN_SKY_COLUMNS, ALL_STATES_URL, DATA_FILE_PATH)
-            >> load_from_file(DATA_FILE_PATH, DB_FILE_PATH)
+            >> get_flight_data(OPEN_SKY_COLUMNS, ALL_STATES_URL)
+            >> load_from_file(DB_FILE_PATH)
             >> [check_row_numbers(DB_FILE_PATH), check_duplicates(DB_FILE_PATH)]
             >> EmptyOperator(task_id="end")
     )
