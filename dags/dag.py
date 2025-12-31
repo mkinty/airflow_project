63d5546ba8a549a8e2a3f797aab01948755685c6
Moustapha KINTY <kintymoustapha@gmail.com>
2025-12-31 17:00:36 +0100

    les task groug, les task group et xcom, generation dynamique des dags

--- dags/dag.py
+++ dags/dag.py
@@ -3,7 +3,7 @@ import time
 from datetime import datetime
 
 import requests
-from airflow.decorators import task
+from airflow.decorators import task, task_group
 from airflow.models import Param
 from airflow.models.dag import dag
 from airflow.operators.empty import EmptyOperator
@@ -12,50 +12,51 @@ from airflow.sensors.filesystem import FileSensor
 
 # Télécharger les données open sky
 
-endpoint_to_params = {
-    "states": {
-        "url": "https://opensky-network.org/api/states/all?extended=true",
-        "columns": [
-            "icao24",
-            "callsign",
-            "origin_country",
-            "time_position",
-            "last_contact",
-            "longitude",
-            "latitude",
-            "baro_altitude",
-            "on_ground",
-            "velocity",
-            "true_track",
-            "vertical_rate",
-            "sensors",
-            "geo_altitude",
-            "squawk",
-            "spi",
-            "position_source",
-            "category"],
-        "target_table": "bdd_airflow.main.openskynetwork_brute",
-        "timestamp_required": False
-
-    },
-    "flights": {
-        "url": "https://opensky-network.org/api/flights/all?begin={begin}&end={end}",
-        "columns": ['icao24',
-                    'firstSeen',
-                    'estDepartureAirport',
-                    'lastSeen',
-                    'estArrivalAirport',
-                    'callsign',
-                    'estDepartureAirportHorizDistance',
-                    'estDepartureAirportVertDistance',
-                    'estArrivalAirportHorizDistance',
-                    'estArrivalAirportVertDistance',
-                    'departureAirportCandidatesCount',
-                    'arrivalAirportCandidatesCount'],
-        "target_table": "bdd_airflow.main.flights_brute",
-        "timestamp_required": True
-    }
-}
+apis_list = [
+    {"schedule": "30 9 * * *",
+     "name": "states",
+     "url": "https://opensky-network.org/api/states/all?extended=true",
+     "columns": [
+         "icao24",
+         "callsign",
+         "origin_country",
+         "time_position",
+         "last_contact",
+         "longitude",
+         "latitude",
+         "baro_altitude",
+         "on_ground",
+         "velocity",
+         "true_track",
+         "vertical_rate",
+         "sensors",
+         "geo_altitude",
+         "squawk",
+         "spi",
+         "position_source",
+         "category"],
+     "target_table": "bdd_airflow.main.openskynetwork_brute",
+     "timestamp_required": False,
+     },
+    {"schedule": "30 14 * * *",
+     "name": "flights",
+     "url": "https://opensky-network.org/api/flights/all?begin={begin}&end={end}",
+     "columns": ['icao24',
+                 'firstSeen',
+                 'estDepartureAirport',
+                 'lastSeen',
+                 'estArrivalAirport',
+                 'callsign',
+                 'estDepartureAirportHorizDistance',
+                 'estDepartureAirportVertDistance',
+                 'estArrivalAirportHorizDistance',
+                 'estArrivalAirportVertDistance',
+                 'departureAirportCandidatesCount',
+                 'arrivalAirportCandidatesCount'],
+     "target_table": "bdd_airflow.main.flights_brute",
+     "timestamp_required": True,
+     }
+]
 
 
 # Chemins absolus dans le container
@@ -85,11 +86,11 @@ def format_datetime(input_datetime):
 
 
 @task(multiple_outputs=True)
-def run_parameters(params=None, dag_run=None):
+def run_parameters(api, dag_run=None):
     data_interval_start = format_datetime(dag_run.data_interval_start)
     data_interval_end = format_datetime(dag_run.data_interval_end)
 
-    out = endpoint_to_params[params["endpoint"]]
+    out = api
     if out["timestamp_required"]:
         end = int(time.time())
         begin = end - 3600
@@ -100,10 +101,14 @@ def run_parameters(params=None, dag_run=None):
     return out
 
 
+@task_group
+def data_ingestion_tg(run_params):
+    get_flight_data(run_params) >> load_from_file()
+
+
 @task(multiple_outputs=True)
-def get_flight_data(ti=None):
+def get_flight_data(run_params):
     # Retrouve les paramètres de la tâche run_parameters
-    run_params = ti.xcom_pull(task_ids="run_parameters", key="return_value")
     url = run_params["url"]
     columns = run_params["columns"]
     data_file_name = run_params["data_file_name"]
@@ -143,10 +148,16 @@ def load_from_file():
     )
 
 
+@task_group
+def data_quality_tg():
+    check_row_numbers()
+    check_duplicates()
+
+
 @task()
 def check_row_numbers(ti=None):
-    expected_lines = ti.xcom_pull(task_ids='get_flight_data', key='rows')
-    lines_found = ti.xcom_pull(task_ids='load_from_file', key='return_value')[0][0]
+    expected_lines = ti.xcom_pull(task_ids='data_ingestion_tg.get_flight_data', key='rows')
+    lines_found = ti.xcom_pull(task_ids='data_ingestion_tg.load_from_file', key='return_value')[0][0]
 
     if lines_found != expected_lines:
         raise Exception(f"Nombre de lignes chargees ({lines_found}) != nombre de lignes de l'API ({expected_lines})")
@@ -164,34 +175,23 @@ def check_duplicates():
     )
 
 
-@dag(
-    params={
-        "endpoint": Param(
-            default="states",
-            enum=list(endpoint_to_params.keys())
-        )
-    },
-    start_date=datetime(2025, 12, 1),
-    schedule_interval="0 0 * * *",
-    catchup=False,
-    concurrency=1,
-)
-def flights_pipeline():
-    (
-            EmptyOperator(task_id="start")
-            >> FileSensor(
-                task_id="waite_for_file",
-                fs_conn_id="file_conn",
-                filepath="/opt/airflow/dags/new_data/{{ params.endpoint }}.json",
-                poke_interval=120,
-                mode="reschedule",
-                timeout=600)
-            >> run_parameters()
-            >> get_flight_data()
-            >> load_from_file()
-            >> [check_row_numbers(), check_duplicates()]
-            >> EmptyOperator(task_id="end")
+for api in apis_list:
+    @dag(
+        dag_id=api["name"],
+        start_date=datetime(2025, 12, 1),
+        schedule_interval=api["schedule"],
+        catchup=False,
+        concurrency=1,
     )
+    def flights_pipeline():
+        run_parameters_task = run_parameters(api)
+        (
+                EmptyOperator(task_id="start")
+                >> run_parameters_task
+                >> data_ingestion_tg(run_params=run_parameters_task)
+                >> data_quality_tg()
+                >> EmptyOperator(task_id="end")
+        )
 
 
-flight_pipeline_dag = flights_pipeline()
+    flights_pipeline()
